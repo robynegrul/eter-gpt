eb9491e6340c0fffc0f83d597214f586a1783582
Noje Vlad <vlad.noje@student.unitbv.ro>
2024-12-29 14:57:47 +0200

    modificare qt iar

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-﻿#include "Game.h"
+﻿/*#include "Game.h"
 #include <iostream>
 
 int main() {
@@ -10,5 +10,37 @@ int main() {
     game.StartGame();
 
     return 0;
+}*/
+
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QMessageBox>
+
+int main(int argc, char* argv[])
+{
+    // Crează aplicația Qt
+    QApplication app(argc, argv);
+
+    // Crează o fereastră principală
+    QWidget window;
+
+    // Setează dimensiunile ferestrei (mai mari)
+    window.resize(600, 400);
+    window.setWindowTitle("Fereastra Qt cu Buton");
+
+    // Crează un buton
+    QPushButton* button = new QPushButton("Apasă-mă!", &window);
+    button->setGeometry(250, 170, 100, 40);  // Plasează butonul în mijlocul ferestrei
+
+    // Conectează butonul la o funcție care închide fereastra când este apăsat
+    QObject::connect(button, &QPushButton::clicked, &app, &QApplication::quit);
+
+    // Afișează fereastra
+    window.show();
+
+    // Rulează aplicația Qt
+    return app.exec();
 }
+
 
